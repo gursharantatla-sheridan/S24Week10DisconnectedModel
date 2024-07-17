4e720226180dc31ff44b54fad00a124ef7c60395
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2024-07-17 08:44:35 -0400

    data class - get all products

--- S24Week10DisconnectedModel/Data.cs
+++ S24Week10DisconnectedModel/Data.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace S24Week10DisconnectedModel
 {
@@ -13,6 +15,21 @@ namespace S24Week10DisconnectedModel
         public static string ConnectionString
         {
             get { return connStr; }
+        }
+
+        public DataTable GetAllProducts()
+        {
+            SqlConnection conn = new SqlConnection(ConnectionString);
+
+            string query = "select ProductID, ProductName, UnitPrice, UnitsInStock from Products";
+            SqlDataAdapter adp = new SqlDataAdapter(query, conn);
+
+            DataSet ds = new DataSet();
+
+            adp.Fill(ds);
+            DataTable tbl = ds.Tables[0];
+
+            return tbl;
         }
     }
 }
